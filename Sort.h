025b84f0ec20b4da967ed793a045890115b9d906
burdey <88636823+burdey@users.noreply.github.com>
2022-10-10 00:14:35 +0800

    Add files via upload

---
+++ Sort.h
@@ -0,0 +1,23 @@
+//
+// Created by Satori on 2022/10/9.
+//
+
+#ifndef P7_SORT_H
+#define P7_SORT_H
+#include <vector>
+
+class Sort{
+public:
+    Sort(){};
+    virtual std::vector<int> sort(){};
+    virtual void sort(std::vector<int>){}; //Actually I prefer to use pure virtual functions, but I still can't build a good list
+    void swap(int&a, int&b){  //Here we can pre-fabricate a function and save it in other files.
+        int temp = a;
+        a = b;
+        b = temp;
+    };
+    ~Sort(){};
+};
+
+
+#endif //P7_SORT_H
